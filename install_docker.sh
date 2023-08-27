f6faf6d5397188351ae29b47f977bd07ebde3250
akhilsha <akhilsha@amdocs.com>
2023-08-27 23:22:00 +0530

    docker an djenkins added

---
+++ install_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker
+sudo yum install docker-compose-plugin
